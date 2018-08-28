2cd0266b3cc232630cf2ac35c706ba6e8b1e0c09
ManikantaJNSVV <jnsvvmk@gmail.com>
2018-08-28 06:59:09 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,31 @@
+import { Component ,ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class AppComponent {
+  newServerName='';
+  newServerContent='';
+
+  serverElements = [];
+
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+}
